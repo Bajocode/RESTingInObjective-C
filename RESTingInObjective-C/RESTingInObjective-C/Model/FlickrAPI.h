475a1514ab68bc3a90f2975f84be81a07d572a43
Fabijan Bajo <bajo09@gmail.com>
2017-05-30 09:53:33 +0200

    Add FlickrAPI code

---
+++ RESTingInObjective-C/RESTingInObjective-C/Model/FlickrAPI.h
@@ -0,0 +1,15 @@
+//
+//  FlickrAPI.h
+//  RESTingInObjective-C
+//
+//  Created by Fabijan Bajo on 30/05/2017.
+//  Copyright © 2017 Fabijan Bajo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FlickrAPI : NSObject
+
++ (FlickrAPI *)sharedInstance;
+
+@end
---
+++ RESTingInObjective-C/RESTingInObjective-C/Model/FlickrAPI.m
@@ -0,0 +1,48 @@
+//
+//  FlickrAPI.m
+//  RESTingInObjective-C
+//
+//  Created by Fabijan Bajo on 30/05/2017.
+//  Copyright © 2017 Fabijan Bajo. All rights reserved.
+//
+
+#import "FlickrAPI.h"
+
+
+@implementation FlickrAPI
+
+#pragma mark - Constants
+
+const NSString *baseURLString = @"https://api.flickr.com/services/rest";
+const NSString *apiKey = @"a6d819499131071f158fd740860a5a88";
+
+
+#pragma mark - Properties
+
++ (FlickrAPI *)sharedInstance {
+    static dispatch_once_t pred = 0;
+    __strong static FlickrAPI *_sharedObject = nil;
+    // Initialize once
+    dispatch_once(&pred, ^{
+        _sharedObject = [[self alloc] init];
+    });
+    return _sharedObject;
+}
+
+- (NSDateFormatter *)formatter {
+    static dispatch_once_t pred = 0;
+    __strong static NSDateFormatter *_formatter = nil;
+    // Initialize once
+    dispatch_once(&pred, ^{
+        _formatter = [[NSDateFormatter alloc] init];
+        _formatter.dateFormat = @"";
+    });
+    return _formatter;
+}
+
+
+#pragma mark - Methods
+
+
+
+@end
